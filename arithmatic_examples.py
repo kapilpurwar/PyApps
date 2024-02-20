c7f1d72c0bb5c63dd310ec303cc74af549bf1d4a
kpurwawp <kapilpurwar@gmail.com>
2024-02-20 20:23:12 +0530

    First Commit

---
+++ arithmatic_examples.py
@@ -0,0 +1,20 @@
+add1 = 2 + 4
+sub1 = 5 - 3
+add2 = add1 - sub1
+mult1 = 4 * 5
+div1 = 20 / 4
+print(add1)
+print(sub1)
+print(add2)
+print(mult1)
+print(div1)
+
+add3 = 1.23 + 1.89
+print(add3)
+print(round(add3), 1)
+
+a=10.2
+
+print(type(a))
+
+
